4607c213d101325f3ccd8bc782ae490ca3eff8f7
pimpim_iwi <pimpim_iwi@MacBook-Air-khxng-pimpim-iwi.local>
2024-03-10 03:13:23 +0700

    first commit

--- api/photo.ts
+++ api/photo.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import { conn , queryAsync } from "../dbconnect";
-import mysql from "mysql";
-import { Photo } from "../model/user";
 
 
 export const router = express.Router();
 
 router.get("/", (req, res) => {
-    conn.query('SELECT * FROM photo ORDER BY sumscore DESC;', (err, result)=>{
+    conn.query('SELECT photo.*, users.* FROM photo INNER JOIN users ON photo.userID = users.userID ORDER BY photo.sumscore DESC;', (err, result)=>{
       if (err) {
         console.error(err);
         res.status(500).json({ message: "Internal server error" });
@@ -17,32 +15,15 @@ router.get("/", (req, res) => {
     });
   });
 
-
-//   router.put("/:photoID", async (req, res) => {
-//     //receive data
-//            let  id = req.params.photoID;
-//            let sumscore : Photo = req.body;
-           
-//     //        //Get Original data
-//             let sql =mysql.format( "select * from photo where photoID = ?",[id]);
-//             let result = await queryAsync(sql);
-//             const jsonStr = JSON.stringify(result);
-//             const jsonOBj = JSON.parse(jsonStr);
-//             const sumscoreOriginal : Photo = jsonOBj[0];
-            
-          
-//            const updatesumscore = {...sumscoreOriginal, ...sumscore};
-  
-    
-//            sql =
-//            "update  photo set sumscore`=? where photoID`=?";
-//          sql = mysql.format(sql, [
-//           updatesumscore.sumscore,
-//           id
-//          ]);
-//          conn.query(sql, (err, result) => {
-//            if (err) throw err;
-//            res.status(201).json({ affected_row: result.affectedRows });
-//          });
-//      });
-
+router.delete("/:photoID", (req, res) => {
+    const photoID = req.params.photoID;
+    const sql = "DELETE FROM `movie` WHERE `movieID` = ?";
+    conn.query(sql, [photoID], (err, result) => {
+        if (err) {
+            console.error("Error deleting Photo:", err);
+            res.status(500).json({ error: "Internal Server Error" });
+        } else {
+            res.status(200).json({ message: "Photo deleted successfully" });
+        }
+    });
+});
