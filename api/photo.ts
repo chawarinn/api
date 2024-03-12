d5b0d44c3b725e02aa073053a0fab7df97190833
athicsmsu <65011212145@msu.ac.th>
2024-03-12 23:45:50 +0700

    bbb

--- api/photo.ts
+++ api/photo.ts
@@ -1,16 +1,14 @@
 import express from "express";
 import { conn , queryAsync } from "../dbconnect";
-
+import multer from "multer";
+import mysql from "mysql";
 
 export const router = express.Router();
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, deleteObject, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCOBbSH5eIGRAqRlmuUk0PZFFVz8lPZXlM",
   authDomain: "playlist-award-918c1.firebaseapp.com",
@@ -23,16 +21,52 @@ const firebaseConfig = {
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
+const storage = getStorage();
 
 router.get("/", (req, res) => {
-    conn.query('SELECT rankUpdate.*, photo.*, users.*  FROM rankUpdate INNER JOIN photo ON rankUpdate.photoID = photo.photoID INNER JOIN users ON photo.userID = users.userID ORDER BY rankUpdate.score DESC', (err, result)=>{
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: "Internal server error" });
-        return;
-      }
-      res.json(result);
-    });
+  conn.query('SELECT rankUpdate.*, photo.*, users.*  FROM rankUpdate INNER JOIN photo ON rankUpdate.photoID = photo.photoID INNER JOIN users ON photo.userID = users.userID ORDER BY rankUpdate.score DESC', (err, result)=>{
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+      return;
+    }
+    res.json(result);
   });
+});
 
+class FileMiddleware {
+  filename = "";
+  public readonly diskLoader = multer({
+    storage: multer.memoryStorage(),
+
+    limits: {
+      fileSize: 67108864, // 64 MByte
+    },
+  });
+}
+
+const fileUpload = new FileMiddleware();
+
+router.post("/:id", fileUpload.diskLoader.single("file"), async (req, res) => {
+    const userID = req.params.id;
+
+    const filename = Math.round(Math.random() * 10000) + ".png";
+    const storageRef = ref(storage,"/images/"+filename);    
+    const metaData = { contentType : req.file!.mimetype };
+    const snapshot = await uploadBytesResumable(storageRef,req.file!.buffer,metaData)
+    const url = await getDownloadURL(snapshot.ref);
+
+    let sql = "INSERT INTO `photo`(`userID`, `photo_url`) VALUES (?,?)";
+    sql = mysql.format(sql, [
+      userID,
+        url
+    ]);
+    conn.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(201).json({
+            affected_row: result.affectedRows, 
+            last_idx: result.insertId
+        });
+    });
+});
 
